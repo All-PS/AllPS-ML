367341a248d2263349be5250d30910846aca89ab
1Bronze <khdkkhdd@gmail.com>
2024-02-20 21:39:30 +0900

    feat: 대충 flask api...

--- app/ML/RecommendProblem/ML_recommend_use_model.py
+++ app/ML/RecommendProblem/ML_recommend_use_model.py
@@ -2,33 +2,10 @@ from app.util.DatabaseConnection import DatabaseConnection
 import pandas as pd
 import ast
 from joblib import load
+from flask import Flask, request, jsonify
 
 
-df = pd.read_csv('problem_information.csv')
-
-# categories 열을 실제 리스트 형태로 변환
-df['categories'] = df['categories'].apply(lambda x: ast.literal_eval(x))
-
-# 저장된 모델을 파일로부터 불러오기
-nn = load('nn_model.joblib')
-
-# 저장된 MultiLabelBinarizer 인스턴스 불러오기
-mlb_loaded = load('mlb.joblib')
-
-# 저장된 원-핫 인코딩된 데이터 불러오기
-categories_encoded = load('categories_encoded.joblib')
-
-DatabaseConnection.startTransaction()
-
-cursor = DatabaseConnection().cursor()
-
-input_id = int(input("원하는 문제의 ID 입력 : "))
-
-input_select = int(input("원하는 기능 입력(1: 유사한 문제들 중 난이도가 유사한 문제 추천, 2: 유사한 문제들 중 난이도가 더 어려운 문제 추천) : "))
-
-
-def print_problem_info(problem_id):
-
+def print_problem_info(problem_id, cursor):
     select_sql = f"""
         SELECT
             problem.name AS problem_name,
@@ -43,13 +20,16 @@ def print_problem_info(problem_id):
 
     cursor.execute(select_sql)
     result = cursor.fetchone()
-    if result :
-        print("\n------------------")
-        print(f"문제 이름 : {result[0]} \n문제 난이도 ID : {result[2]} \n문제 카테고리 ID : {result[3]}")
-        print("------------------\n")
+
+    if result:
+        print(problem_id, sep=" ")
+        return problem_id
+
+    return None
 
 
-def recommend_similar_problems(problem_id, max_neighbors=30, max_recommendations=5):
+def recommend_similar_problems(problem_id, max_neighbors=30, max_recommendations=5, df=None, categories_encoded=None,
+                               nn=None, input_select=None):
     # 주어진 문제의 인덱스 찾기
     try:
         # 주어진 문제의 인덱스 찾기
@@ -75,7 +55,8 @@ def recommend_similar_problems(problem_id, max_neighbors=30, max_recommendations
     if input_select == 1:
         filtered_indices = [j for j, diff in enumerate(similar_difficulties) if abs(diff - problem_difficulty) <= 2]
     else:
-        filtered_indices = [j for j, diff in enumerate(similar_difficulties) if diff - problem_difficulty > 0 and diff - problem_difficulty <= 10]
+        filtered_indices = [j for j, diff in enumerate(similar_difficulties) if
+                            diff - problem_difficulty > 0 and diff - problem_difficulty <= 10]
     filtered_ids = similar_problem_ids[filtered_indices]
 
     # 거리에 따라 정렬된 상태이므로, 최대 추천 개수만큼 잘라서 반환
@@ -84,14 +65,41 @@ def recommend_similar_problems(problem_id, max_neighbors=30, max_recommendations
     return recommended_ids
 
 
-print("\n입력된 문제의 정보")
-print_problem_info(input_id)
+app = Flask(__name__)
 
-recommended_problem_ids = recommend_similar_problems(input_id)
-i = 1
-for recommend_id in recommended_problem_ids:
-    print(f"{i}번째로 추천된 문제의 정보")
-    print_problem_info(recommend_id)
-    i += 1
 
-DatabaseConnection().close()
+@app.route('/api')
+def home():
+    df = pd.read_csv('problem_information.csv')
+
+    # categories 열을 실제 리스트 형태로 변환
+    df['categories'] = df['categories'].apply(lambda x: ast.literal_eval(x))
+
+    # 저장된 모델을 파일로부터 불러오기
+    nn = load('nn_model.joblib')
+
+    # 저장된 MultiLabelBinarizer 인스턴스 불러오기
+    mlb_loaded = load('mlb.joblib')
+
+    # 저장된 원-핫 인코딩된 데이터 불러오기
+    categories_encoded = load('categories_encoded.joblib')
+
+    cursor = DatabaseConnection().cursor()
+
+    input_id = int(request.args.get('id'))
+    print(input_id, sep=" ")
+    # 1: 유사한 문제들 중 난이도가 유사한 문제 추천, 2: 유사한 문제들 중 난이도가 더 어려운 문제 추천)
+    input_select = int(request.args.get('type'))
+    print(input_select, sep=" ")
+
+    print("\n입력된 문제의 정보")
+    print_problem_info(input_id, cursor)
+
+    recommended_problem_ids = recommend_similar_problems(input_id, 30, 5, df, categories_encoded, nn, input_select).tolist()
+
+    print(recommended_problem_ids)
+    # return 'OK'
+    return jsonify(recommended_problem_ids)
+
+
+app.run()
